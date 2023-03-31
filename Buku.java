bf2c0d55c4954050ad28a0c71fb4f0708bad9cb6
supiansauri <114654547+supiansauri@users.noreply.github.com>
2023-03-31 17:17:58 +0700

    Add files via upload

---
+++ Buku.java
@@ -0,0 +1,17 @@
+public class Buku {
+    public String judul;
+    public String pengarang;
+    public String penerbit;
+    public int tahun ;
+    public int kategori;
+    public String keterangan;
+
+    public Buku(String judul, String pengarang, String penerbit, int tahun, int kategori, String keterangan){
+        this.judul = judul;
+        this.pengarang = pengarang;
+        this.penerbit = penerbit;
+        this.tahun = tahun;
+        this.kategori = kategori;
+        this.keterangan = keterangan;
+    }
+}
